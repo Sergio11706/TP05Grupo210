d64cc05ad04343f194fdffccd10dd1ff07a5401d
sergi <sergi@LAPTOP-K2ASHF92>
2024-06-13 15:36:28 -0300

    Inicializacion del TP05

---
+++ TP05Grupo210/src/main/java/ar/edu/unju/fi/Tp05Grupo210Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp05Grupo210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp05Grupo210Application.class, args);
+	}
+
+}

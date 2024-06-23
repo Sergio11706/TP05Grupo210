4fa248668376dd04cca100a2ab0991d67398f0fd
chano <chano@LAPTOP-VDHN2S85>
2024-06-23 02:13:56 -0300

    Creacion del formulario y el listado de docentes.

--- TP05Grupo210/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ TP05Grupo210/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -1,5 +1,21 @@
 package ar.edu.unju.fi.model;
 
-public class Docente {
+import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Component
+@Entity
+public class Docente {
+	
+	@Id
+	private String legajo;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private boolean estado;
 }
